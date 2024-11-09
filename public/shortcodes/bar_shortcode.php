411adffa31e756160f30436058902a59b02601af
riccardodicurti <info@riccardodicurti.it>
2024-11-09 11:00:06 +0100

    Plugin Check (PCP) fixes

--- public/shortcodes/bar_shortcode.php
+++ public/shortcodes/bar_shortcode.php
@@ -7,7 +7,7 @@ function create_wp_simple_booking_bar( $atts = [], $content = null, $tag = '' ) 
     
     $options = shortcode_atts( $options, $atts, $tag );
 
-    $options['js_bar_settings'] = json_encode( json_decode( $options['js_bar_settings'] ), JSON_UNESCAPED_UNICODE );
+    $options['js_bar_settings'] = json_decode( $options['js_bar_settings'] );
 
     ob_start();
     
@@ -18,12 +18,12 @@ function create_wp_simple_booking_bar( $atts = [], $content = null, $tag = '' ) 
                 echo '(i[r].q = i[r].q || []).push(arguments)';
                 echo '}, i[r].l = 1 * new Date(); a = s.createElement(o),';
                 echo  'm = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)';
-                echo "})(window, document, 'script', 'https://cdn.simplebooking.it/search-box-script.axd?IDA={$options['license_code']}','SBSyncroBox');";
+                echo "})(window, document, 'script', 'https://cdn.simplebooking.it/search-box-script.axd?IDA=" . esc_attr( $options['license_code'] ) . "','SBSyncroBox');";
 
         echo 'SBSyncroBox({';
-            echo "CodLang: '{$options['language_code']}',";
+            echo "CodLang: '" . esc_attr( $options['language_code'] ) . "',";
             echo "MainContainerId: 'sb-container-s',";
-            echo "Styles: {$options['js_bar_settings']},";
+            echo "Styles: " . json_encode( $options['js_bar_settings'], JSON_UNESCAPED_UNICODE ) . ",";
         echo '});';
     echo '</script>';
 
--- public/shortcodes/form_shortcode.php
+++ public/shortcodes/form_shortcode.php
@@ -9,7 +9,7 @@ function create_wp_simple_booking_form( $atts = [], $content = null, $tag = '' )
     $options = shortcode_atts( $options, $atts, $tag );
 
     $options['form_name'] = $options['form_name'] ?? 'RichiestaPreventivoDaSitoWeb';
-    $options['js_bar_settings'] = json_encode( json_decode( $options['js_bar_settings'] ), JSON_UNESCAPED_UNICODE );
+    $options['js_bar_settings'] = json_decode( $options['js_bar_settings'] );
 
     ob_start();
     
@@ -20,17 +20,17 @@ function create_wp_simple_booking_form( $atts = [], $content = null, $tag = '' )
                 echo '(i[r].q = i[r].q || []).push(arguments)';
                 echo '}, i[r].l = 1 * new Date(); a = s.createElement(o),';
                 echo  'm = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)';
-                echo "})(window, document, 'script', 'https://cdn.simplebooking.it/search-box-script.axd?IDA={$options['license_code']}','SBSyncroBox');";
+                echo "})(window, document, 'script', 'https://cdn.simplebooking.it/search-box-script.axd?IDA=" . esc_attr( $options['license_code'] ) . "','SBSyncroBox');";
 
         echo 'SBSyncroBox({';
-            echo "CodLang: '{$options['language_code']}',";
+            echo "CodLang: '" . esc_attr( $options['language_code'] ) . "',";
             echo 'Reference: "sbSyncroBox",';
             echo "MainContainerId: 'sb-converto-container-s',";
-            echo "Styles: {$options['js_bar_settings']},";
+            echo "Styles: " . json_encode( $options['js_bar_settings'], JSON_UNESCAPED_UNICODE ) . ",";
             echo "Converto: {";
                 echo 'InPageContainerId: "sb-converto-container-s",';
-                echo "ThankYouPage: '{$options['thank_you_page']}',";
-                echo "FormName: '{$options['form_name']}'";
+                echo "ThankYouPage: '" . esc_url( $options['thank_you_page'] ) . "',";
+                echo "FormName: '" . esc_attr( $options['form_name'] ) . "'";
             echo "}";
         echo '});';
     echo '</script>';
