3d31df3f5e8b88fd406db1d4dc541a9070aae61e
Riccardo Di Curti <info@riccardodicurti.it>
2024-12-04 16:01:34 +0100

    Update wp_simple_booking.php

--- wp_simple_booking.php
+++ wp_simple_booking.php
@@ -19,31 +19,31 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 
-function wp_simple_booking_acf_init() {
+function rdc_wsb_acf_init() {
 	if ( is_admin() ) {
 		require_once 'admin/settings_customfields.php';
 	}
 }
 
-function wp_simple_booking_get_sburl_by_license_code( $license_code, $language ) {
+function rdc_wsb_get_sburl_by_license_code( $license_code, $language ) {
 	return "https://www.simplebooking.it/ibe/search?hid={$license_code}&lang={$language}&cur=#";
 }
 
-function wp_simple_booking_get_richiediurl_by_license_code( $page_id, $language ) {
+function rdc_wsb_get_richiediurl_by_license_code( $page_id, $language ) {
 	return apply_filters( 'wpml_permalink', get_the_permalink( $page_id ), strtolower( $language ) );
 }
 
-function wp_simple_booking_get_dependencies() {
+function rdc_wsb_get_dependencies() {
 	global $post;
 	$options = [];
 
 	$wordpress_simple_booking_options = get_option( 'wordpress_simple_booking_option_name' );
 
-	$options['mobile_theme_version'] = $wordpress_simple_booking_options['mobile_theme_version'] ?? '';
-	$options['license_code'] = apply_filters( 'wp_simple_booking/wp_simple_booking_get_dependencies/default_license_code_0', $wordpress_simple_booking_options['default_license_code_0'] ?? '0000' );
-	$options['availability_locale'] = $wordpress_simple_booking_options['default_availability_locale_1'] ?? '';
-	$options['language_code'] = $wordpress_simple_booking_options['default_language_code_2'] ?? '';
-	$options['js_bar_settings'] = $wordpress_simple_booking_options['js_bar_settings'] ?? '';
+	$options['mobile_theme_version'] = $wordpress_simple_booking_options['mobile_theme_version'];
+	$options['license_code'] = $wordpress_simple_booking_options['default_license_code_0'];
+	$options['availability_locale'] = $wordpress_simple_booking_options['default_availability_locale_1'];
+	$options['language_code'] = $wordpress_simple_booking_options['default_language_code_2'];
+	$options['js_bar_settings'] = $wordpress_simple_booking_options['js_bar_settings'];
 
 	if ( defined( 'ICL_LANGUAGE_CODE' ) ) {
 		$options['language_code'] = strtoupper( defined( 'ICL_LANGUAGE_CODE' ) );
@@ -58,39 +58,37 @@ function wp_simple_booking_get_dependencies() {
 		$options['language_code'] = strtoupper( get_field( 'language_code', $post->ID ) ) ?: $options['language_code'];
 	}
 
-	$options['prenota_url'] = wp_simple_booking_get_sburl_by_license_code( $wordpress_simple_booking_options['default_license_code_0'], $options['language_code'] ); 
-	$options['richiedi_url'] = wp_simple_booking_get_richiediurl_by_license_code( $wordpress_simple_booking_options['request_page_id'], $options['language_code'] ); 
-	$options['prenota_label'] = __( 'Prenota', 'wp_simple_booking' ); 
-	$options['richiedi_label'] = __( 'Richiedi', 'wp_simple_booking' );
+	$options['prenota_url'] = rdc_wsb_get_sburl_by_license_code( $wordpress_simple_booking_options['default_license_code_0'], $options['language_code'] ); 
+	$options['richiedi_url'] = rdc_wsb_get_richiediurl_by_license_code( $wordpress_simple_booking_options['request_page_id'], $options['language_code'] ); 
+	$options['prenota_label'] = __( 'Prenota', 'rdc_wsb' ); 
+	$options['richiedi_label'] = __( 'Richiedi', 'rdc_wsb' );
 
 	return $options;
 }
 
-function wp_simple_booking_enqueue_dependencies() {
-	$options = wp_simple_booking_get_dependencies();
-	$plugin_data = get_plugin_data(  __FILE__ ); 
+function rdc_wsb_enqueue_dependencies() {
+	$options = rdc_wsb_get_dependencies();
 
-	if ( $options['license_code'] != '0000' ) {
-		wp_enqueue_style( 'wp_simple_booking_style', plugin_dir_url( __FILE__ ) . 'public/css/simple_booking_style.css', [] , $plugin_data['Version'] );
-		wp_register_script( 'wp_simple_booking_scripts', plugin_dir_url( __FILE__ ) . 'public/js/simplebooking.js', [ 'jquery' ], $plugin_data['Version'], [ 'strategy'  => 'defer', 'in_footer' => true ] );
+	if ( $options['license_code'] ) {
+		wp_enqueue_style( 'rdc_wsb_style', plugin_dir_url( __FILE__ ) . 'public/css/simple_booking_style.css' );
+		wp_register_script( 'rdc_wsb_scripts', plugin_dir_url( __FILE__ ) . 'public/js/simplebooking.js', [ 'jquery' ], false, true );
 
-		wp_localize_script( 'wp_simple_booking_scripts', 'options', $options );
-		wp_enqueue_script( 'wp_simple_booking_scripts' );
+		wp_localize_script( 'rdc_wsb_scripts', 'options', $options );
+		wp_enqueue_script( 'rdc_wsb_scripts' );
 	}
 }
 
-function wp_simple_booking_admin_error_notice() {
+function rdc_wsb_admin_error_notice() {
 	global $pagenow;
 
-	if ( $pagenow == 'admin.php' && isset( $_GET['page'] ) && htmlspecialchars( wp_unslash( $_GET['page'] ) ) == 'wordpress-simple-booking' ) {
+	if ( $pagenow == 'admin.php' && isset( $_GET['page'] ) && htmlspecialchars( $_GET['page'] ) == 'wordpress-simple-booking' ) {
 		$install_acf_url = get_site_url( null, '/wp-admin/plugin-install.php?s=Advanced%2520Custom%2520Fields&tab=search&type=term' );
-		// translators: placeholder contain the acf plugin url
-		printf( '<div class="notice error my-acf-notice is-dismissible"><p>' . __( 'Unlock more "WordPress Simple Booking" features installing <a href="%s">Advanced Custom Fields</a>', 'wp_simple_booking' ) . '.</p></div>', esc_url( $install_acf_url ) );
+		printf( '<div class="notice error my-acf-notice is-dismissible"><p>' . __( 'Unlock more "WordPress Simple Booking" features installing <a href="%s">Advanced Custom Fields</a>', 'rdc_wsb' ) . '.</p></div>', $install_acf_url );
 	}
 }
 
-function wp_simple_booking_init() {
-	add_action( 'wp_enqueue_scripts', 'wp_simple_booking_enqueue_dependencies' );
+function rdc_wsb_init() {
+	add_action( 'wp_enqueue_scripts', 'rdc_wsb_enqueue_dependencies' );
 
 	if ( is_admin() ) {
 		require_once 'admin/settings_menupage.php';
@@ -99,14 +97,13 @@ function wp_simple_booking_init() {
 		require_once 'public/shortcodes/form_shortcode.php';
 	}
 
-	if ( ! is_plugin_active( 'advanced-custom-fields/acf.php' ) && 
-		 ! is_plugin_active( 'advanced-custom-fields-pro/acf.php' ) ) {
+	if ( ! is_plugin_active( 'advanced-custom-fields/acf.php' ) ) {
 		if ( is_admin() ) {
-			add_action( 'admin_notices', 'wp_simple_booking_admin_error_notice' );
+			add_action( 'admin_notices', 'rdc_wsb_admin_error_notice' );
 		}
 	} else {
-		add_action( 'acf/init', 'wp_simple_booking_acf_init' );
+		add_action( 'acf/init', 'rdc_wsb_acf_init' );
 	}
 }
 
-add_action( 'init', 'wp_simple_booking_init' );
+add_action( 'init', 'rdc_wsb_init' );
